16876c1795cdf1bafe36c8e1ba54516825efd555
oscar-valen115 <oscar.valen115@gmail.com>
2021-03-24 14:14:07 -0400

    Created cart and order navigation items  Successfully tested car and order nav items.  Co-authored-by: Connie Panzariello <connie.panzariello@gmail.com>

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom'
+
+// import { getOrders } from '../../api/auth'
+// import messages from '../AutoDismissAlert/messages'
+
+// import Form from 'react-bootstrap/Form'
+// import Button from 'react-bootstrap/Button'
+
+class Cart extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      oldPassword: '',
+      newPassword: ''
+    }
+  }
+
+  // handleChange = event => this.setState({
+  //   [event.target.name]: event.target.value
+  // })
+
+  // onChangePassword = event => {
+  //   event.preventDefault()
+
+  //   const { msgAlert, history, user } = this.props
+
+  //   changePassword(this.state, user)
+  //     .then(() => msgAlert({
+  //       heading: 'Change Password Success',
+  //       message: messages.changePasswordSuccess,
+  //       variant: 'success'
+  //     }))
+  //     .then(() => history.push('/'))
+  //     .catch(error => {
+  //       this.setState({ oldPassword: '', newPassword: '' })
+  //       msgAlert({
+  //         heading: 'Change Password Failed with error: ' + error.message,
+  //         message: messages.changePasswordFailure,
+  //         variant: 'danger'
+  //       })
+  //     })
+  // }
+
+  render () {
+    // const { oldPassword, newPassword } = this.state
+
+    return (
+      <div className="row">
+        <h1>Cart Page</h1>
+      </div>
+    )
+  }
+}
+
+export default withRouter(Cart)
