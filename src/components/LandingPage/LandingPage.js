b4dcc74b321779afc790ca09637785331f043058
sven-gerlach <svengerlach@me.com>
2021-03-24 14:31:56 -0400

    Refactor product state Moved product state from APP to LandingPage component.

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -2,8 +2,27 @@ import React, { Component } from 'react'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import { getProductsFromApi } from '../../api/products'
 
 class LandingPage extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      products: []
+    }
+  }
+
+  componentDidMount () {
+    // make axios call to set the products state
+    getProductsFromApi()
+      .then(response => {
+        this.setState({
+          products: response.data.products
+        })
+      })
+      .catch(console.error)
+  }
+
   render () {
     return (
       <Container fluid>
