cc55687dff01043cc1df009700264dee328df6b3
sven-gerlach <svengerlach@me.com>
2021-03-24 14:13:30 -0400

    Get products from api Implemented getProductsFromApi function which loads all products and stores them in the product state of the APP component.

---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+class LandingPage extends Component {
+  render () {
+    return (
+      <Container fluid>
+        <Row>
+          <Col>
+            <p>Test</p>
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
+}
+
+export default LandingPage
