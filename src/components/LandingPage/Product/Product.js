1285d02a363667cd6ee5e90658ee3e1ebe90c292
sven-gerlach <svengerlach@me.com>
2021-03-24 16:48:53 -0400

    Refactor product component into class

--- src/components/LandingPage/Product/Product.js
+++ src/components/LandingPage/Product/Product.js
@@ -1,14 +1,14 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
 
 class Product extends Component {
   render () {
     return (
-      <Fragment>
+      <div>
         <p>{this.props.name}</p>
         <p>{this.props.description}</p>
         <p>{this.props.price}</p>
         <p>{this.props.imgUrl}</p>
-      </Fragment>
+      </div>
     )
   }
 }
